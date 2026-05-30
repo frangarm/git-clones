0950eccb08f29d2a6fc9a461d4e6a3941764ace2
frangarm <franciscomgarcia23@gmail.com>
2026-05-30 16:13:17 -0600

    Implemented CLI tools and commit object support
    CLI Integration: Added parsers and command functions for cat-file and hash-object to the main entry point.
    
    Commit Handling: Implemented the GitPyCCommit class along with kvlm_parse and kvlm_serialize to handle the 'Key-Value List with Message' format used in Git commits.
    
    Object Utilities: Added object_hash to wrap the hashing and writing process, and a basic cat_file utility for object retrieval.

--- gitpyc/libgitpyc.py
+++ gitpyc/libgitpyc.py
@@ -28,6 +28,8 @@ def main (argv=sys.argv[1:]):
     
     match args.command:
         case "init": cmd_init(args)
+        case "cat-file": cmd_cat_file(args)
+        case "hash-object": cmd_hash_object(args)
         case _: print("Unkown Command")
 
 class GitPyCRepository:
@@ -176,7 +178,7 @@ def object_read(repo, sha):
         raise Exception(f"Malformed Object {sha}: bad length")
 
     match fmt:
-        case b'commit': c = None
+        case b'commit': c = GitPyCCommit
         case b'tree': c = None
         case b'tag': c = None
         case b'blob': c = GitPyCBlob
@@ -205,3 +207,99 @@ class GitPyCBlob(GitPyCObject):
     
     def deserialize(self, data):
         self.blobdata = data
+
+argsp = argsubparser.add_parser("cat-file", help="Provide content of repository objects.")
+argsp.add_argument("type", metavar="type", choices=["blob", "commit", "tag", "tree"])
+argsp.add_argument("object", metavar="object")
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, #object_find(repo, obj, fmt=fmt))
+    None)
+    sys.stdout.buffer.write(obj.serialize())
+
+argsp = argsubparser.add_parser("hash-object", help="Compute object ID and optionally store a blob from a file")
+argsp.add_argument("-t", metavar="type", dest="type", choices=["blob", "commit", "tag", "tree"], default="blob")
+argsp.add_argument("-w", dest="write", action="store_true")
+argsp.add_argument("path")
+
+def cmd_hash_object(args):
+    repo = repo_find() if args.write else None
+    
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+    
+    match fmt:
+        case b'commit': obj = GitPyCCommit(data)
+        case b'tree': obj = None
+        case b'tag': obj = None
+        case b'blob': obj = GitPyCBlob(data)
+        case _: raise Exception(f"Unknown type: {fmt}")
+    
+    return object_write(obj, repo)
+
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = dict()
+    
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+    
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[None] = raw[start + 1:]
+        return dct
+    
+    key = raw[start:spc]
+    end = start
+    
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '):
+            break
+    
+    value = raw[spc+1:end].replace(b'\n ', b'\n')
+    
+    if key in dct:
+        if type(dct[key]) == list: 
+            dct[key].append(value)
+        else:
+            dct[key] = [dct[key], value]
+    else:
+        dct[key] = value
+    
+    return kvlm_parse(raw, start=end+1, dct=dct)
+
+def kvlm_serialize(kvlm):
+    ret = b''
+    
+    for k in kvlm.keys():
+        if k is None:
+            continue
+        val = kvlm[k]
+        if type(val) != list:
+            val = [val]
+        for v in val:
+            ret += k + b' ' + v.replace(b'\n', b'\n ') + b'\n'
+    ret += b'\n' + kvlm[None]
+    
+    return ret
+
+class GitPyCCommit(GitPyCObject):
+    fmt = b'commit'
+    
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+    
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+    
+    def init(self):
+        self.kvlm = dict()
